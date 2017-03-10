a3a779742a7c2f636d997567a5e40b9812676af8
Artem <artem3m@mail.ru>
2017-03-10 21:24:25 +0300

    Unium.V5

--- sum.cs
+++ sum.cs
@@ -18,14 +18,14 @@ namespace WindowsFormsApplication1
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Double a,n,d=1;
+            Double a,n;
             a = Convert.ToInt32(textBox2.Text);
             n = a;
             if (n < 0)
             {
-                n = n + 2;
+                n = n *-1 + 2;
             }
-            d = d * (n - 1)/2;
+            textBox1.Text = Convert.ToString((a+1)*n/2);
         } 
     }
 }
